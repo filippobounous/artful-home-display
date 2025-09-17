a5252c7ad33e54fd56f332d13bb07821a1fa4590
Filippo Bounous <62483970+filippobounous@users.noreply.github.com>
2025-09-17 12:29:29 +0100

    main: remove outer theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ThemeProvider } from './components/ThemeProvider';
 import { ServiceProvider } from './context/ServiceProvider';
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <ServiceProvider>
-      <App />
-    </ServiceProvider>
-  </ThemeProvider>,
+  <ServiceProvider>
+    <App />
+  </ServiceProvider>,
 );
