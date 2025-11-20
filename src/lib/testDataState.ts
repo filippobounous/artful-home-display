23b18b30492e0359faea92bfcfc7852ec705c8b8
Filippo Bounous <62483970+filippobounous@users.noreply.github.com>
2025-11-20 15:49:59 +0000

    api: use context test data state

---
+++ src/lib/testDataState.ts
@@ -0,0 +1,9 @@
+let testDataEnabled = true;
+
+export function setTestDataState(enabled: boolean) {
+  testDataEnabled = enabled;
+}
+
+export function getTestDataState() {
+  return testDataEnabled;
+}
